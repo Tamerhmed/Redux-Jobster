a3ef780fc071e0a48754575d992aaa9bacb26363
Tamerhmed <tamer30230@gmail.com>
2022-11-13 22:03:33 +1100

    stats page

--- src/features/allJobs/allJobsSlice.js
+++ src/features/allJobs/allJobsSlice.js
@@ -37,6 +37,23 @@ export const getAllJobs = createAsyncThunk('allJobs/getAllJobs', async(_, thunkA
   }
 });
 
+export const showStats = createAsyncThunk('allJobs/showStats',
+ async(_, thunkAPI)=> {
+  try {
+      const resp = await customFetch.get('/jobs/stats', {
+        headers: {
+          authorization :`Bearer ${thunkAPI.getState().user.user.token}`,
+        }
+      });
+      console.log(resp.data);
+      return resp.data;
+  } catch (error) {
+      return thunkAPI.rejectWithValue(error.response.data.msg);
+  }
+})
+
+
+
 const allJobsSlice = createSlice({
   name: 'allJobs',
   initialState,
@@ -55,6 +72,18 @@ const allJobsSlice = createSlice({
     [getAllJobs.fulfilled]: (state, {payload})=> {
       state.isLoading = false;
       state.jobs = payload.jobs;
+    },
+    [getAllJobs.rejected]: (state, {payload})=> {
+      state.isLoading = false;
+      toast.error(payload);
+    },
+    [showStats.pending]: (state)=> {
+      state.isLoading = true;
+    },
+    [showStats.fulfilled]: (state, {payload})=> {
+      state.isLoading = false;
+      state.stats = payload.defaultStats;
+      state.monthlyApplications = payload.monthlyApplications;
     },
     [getAllJobs.rejected]: (state, {payload})=> {
       state.isLoading = false;
